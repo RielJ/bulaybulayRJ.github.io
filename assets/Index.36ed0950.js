9dbb1182f0b401fd0d4ef16ca86d5ce5e565e980
Riel Joseph Bulaybulay <bulaybulay.rielj@gmail.com>
2021-05-26 11:13:53 +0800

    initial commit

---
+++ assets/Index.36ed0950.js
@@ -0,0 +1,1 @@
+import{o,c as r}from"./vendor.e3897788.js";const e={};e.render=function(e,n,t,u,d,s){return o(),r("div",null,"About")};export default e;
---
+++ assets/Index.576b7fea.js
@@ -0,0 +1,1 @@
+import{o as e,c as r}from"./vendor.e3897788.js";const n={};n.render=function(n,o,t,s,u,d){return e(),r("div",null,"Resume")};export default n;
