ae4675935021a22c396027ad4f5e551d9782d7e5
Riel Joseph Bulaybulay <bulaybulay.rielj@gmail.com>
2021-05-26 20:09:50 +0800

    deploy

---
+++ assets/Index.1f3a514e.js
@@ -0,0 +1,1 @@
+import{o as e,c as r}from"./vendor.84e871d1.js";const n={};n.render=function(n,o,t,d,s,u){return e(),r("div",null,"Resume")};export default n;
---
+++ assets/Index.940213c8.js
@@ -0,0 +1,1 @@
+import{o,c as r}from"./vendor.84e871d1.js";const e={};e.render=function(e,n,t,d,u,s){return o(),r("div",null,"About")};export default e;
